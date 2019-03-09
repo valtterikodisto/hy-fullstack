f0e8b222982a8057a625cf60447e7b446813985a
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-03-10 01:32:38 +0200

    Added exercises 6.18 & 6.19

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,17 +2,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addAnecdote } from '../reducers/anecdoteReducer'
 import { showNotification, hideNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
   const add = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    const newAnecdote = await anecdoteService.createNew(content)
-    props.addAnecdote(newAnecdote)
+    event.target.anecdote.value = ''
+    props.addAnecdote(content)
     props.showNotification(`You added '${content}'`)
     setTimeout(() => props.hideNotification(), 5000)
-    //event.target.anecdote.value = ''
   }
 
   return (
--- osa6/redux-anecdotes/src/index.js
+++ osa6/redux-anecdotes/src/index.js
@@ -1,24 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import anecdoteReducer, { initializeAnecdotes } from './reducers/anecdoteReducer'
-import notificationReducer from './reducers/notificationReducer'
-import filterReducer from './reducers/filterReducer'
-import anecdoteService from './services/anecdotes'
-
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  notification: notificationReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer)
-
-anecdoteService.getAll().then(anecdotes => {
-  store.dispatch(initializeAnecdotes(anecdotes))
-})
+import store from './store'
 
 const render = () => {
   ReactDOM.render(
