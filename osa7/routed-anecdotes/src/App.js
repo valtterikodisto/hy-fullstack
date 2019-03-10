8b083a95311e0eba007bf9e0db3012934b784cc5
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-03-10 12:21:10 +0200

    Added exercise 7.2

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -21,8 +21,22 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
+  </div>
+)
+
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{`${anecdote.content}, by ${anecdote.author}`}</h2>
+    <p>{`has ${anecdote.votes} votes`}</p>
+    <p>
+      for more info see <a href={anecdote.info} rel="noopener noreferrer" target="_blank">{anecdote.info}</a>
+    </p>
   </div>
 )
 
@@ -133,6 +147,9 @@ const App = () => {
         <div>
           <Menu />
           <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
+          <Route exact path="/anecdotes/:id" render={({ match }) => 
+            <Anecdote anecdote={anecdoteById(match.params.id)} />
+          } />
           <Route path="/create" render={() => <CreateNew addNew={addNew} />} />
           <Route path="/about" render={() => <About />} />
         </div>
